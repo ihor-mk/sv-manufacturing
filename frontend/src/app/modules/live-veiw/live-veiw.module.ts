ec61bc1fbe26844a239edf2354480ed0f19fe71f
ihor-mk <ihor.myroniuk@hotmail.com>
2023-09-11 16:43:07 +0300

    change header

--- frontend/src/app/modules/live-veiw/live-veiw.module.ts
+++ frontend/src/app/modules/live-veiw/live-veiw.module.ts
@@ -4,11 +4,13 @@ import { LiveVeiwPageComponent } from './live-veiw-page/live-veiw-page.component
 import { SharedModule } from 'src/app/shared/shared.module';
 import { MainModule } from '../main/main.module';
 import { LiveVeiwRoutingModule } from './live-veiw-routing.module';
+import { ProductionLineLiveComponent } from './production-line-live/production-line-live.component';
 
 
 @NgModule({
   declarations: [
-    LiveVeiwPageComponent
+    LiveVeiwPageComponent,
+    ProductionLineLiveComponent
   ],
   imports: [
     CommonModule,
---
+++ frontend/src/app/modules/live-veiw/production-line-live/production-line-live.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-production-line-live',
+  templateUrl: './production-line-live.component.html',
+  styleUrls: ['./production-line-live.component.sass']
+})
+export class ProductionLineLiveComponent {
+
+}

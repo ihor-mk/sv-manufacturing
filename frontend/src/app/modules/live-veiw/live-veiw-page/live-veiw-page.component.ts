505fec6d52ebd92804ee7e4a4f74054eff911462
ihor-mk <ihor.myroniuk@hotmail.com>
2023-09-18 16:13:34 +0300

    add line-live ui

--- frontend/src/app/modules/live-veiw/live-veiw-page/live-veiw-page.component.ts
+++ frontend/src/app/modules/live-veiw/live-veiw-page/live-veiw-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ILine } from 'src/app/models/ILine';
 
 @Component({
   selector: 'app-live-veiw-page',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./live-veiw-page.component.sass']
 })
 export class LiveVeiwPageComponent {
-
+  constructor() {
+    this.lineArray = [this.lineTemplate, this.lineTemplate, this.lineTemplate, this.lineTemplate, this.lineTemplate]
+  }
+  lineTemplate : ILine = {
+    title : 'ЛІНІЯ 1',
+    productivityOnline: 12,
+    productivityTop: 15,
+    nomenclature: 'NOM-17/23',
+    plan: 1000,
+    fact: 237,
+    progress: 0,
+    avgSpeed: 0,
+    time: new Date(),
+    endTime: new Date()
+   }
+  
+   lineArray : ILine[]
 }
--- frontend/src/app/modules/live-veiw/production-line-live/production-line-live.component.ts
+++ frontend/src/app/modules/live-veiw/production-line-live/production-line-live.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ILine } from 'src/app/models/ILine';
 
 @Component({
   selector: 'app-production-line-live',
   templateUrl: './production-line-live.component.html',
   styleUrls: ['./production-line-live.component.sass']
 })
-export class ProductionLineLiveComponent {
+export class ProductionLineLiveComponent implements OnInit {
 
+  @Input() line!: ILine;
+
+
+  ngOnInit(): void {
+    if (!this.line) {
+      console.log("EMPTY")
+    }
+  }
 }

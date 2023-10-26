dbd83f6b4617ba226ac2e429ee33b26d18311f80
ihor-mk <ihor.myroniuk@hotmail.com>
2023-10-26 09:56:44 +0300

    Implement RabbitMQ connection provider

---
+++ backend/SunVita.RabbitMQ/ConnectionProvider.cs
@@ -0,0 +1,16 @@
+﻿using RabbitMQ.Client;
+
+namespace SunVita.RabbitMQ
+{
+    public class ConnectionProvider
+    {
+        public IConnection Connection { get; }
+
+        public ConnectionProvider(string hostname)
+        {
+            var factory = new ConnectionFactory { Uri = new Uri(hostname) };
+            factory.AutomaticRecoveryEnabled = true;
+            Connection = factory.CreateConnection();
+        }
+    }
+}
